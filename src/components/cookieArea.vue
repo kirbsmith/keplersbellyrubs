59baf4143390b1cda5c79af36b66fa7f1e4f23f4
Kirby Smith <kirbsmith@gmail.com>
2022-12-27 08:58:54 -0500

    added random bonus generator and media queries

--- src/components/cookieArea.vue
+++ src/components/cookieArea.vue
@@ -1,34 +1,47 @@
 <template>
     <div class="flex flex-col md:flex-row justify-evenly">
-      <div class="flex flex-col pt-6 pl-6">
+      <div class="flex flex-col pt-6 px-4 md:pl-6">
         <h3 class="text-xl text-center">Click Kepler to collect belly rubs</h3>
         <img src="../assets/kepler.jpg" alt="" class="w-60 self-center py-6 rounded-full" @click="bellyCounter++"/>
         <p class="text-lg text-center py-2">Belly Rubs: {{ bellyCounter }}</p>
         <div class="flex flex-col border-solid border-2 border-black rounded-md mb-6">
-          <p class="text-md text-center py-2">Auto Belly Rubs Per Second: {{ autoScratcher/2 }}</p>
-          <p class="text-md text-center py-2">Latex Glove Rubs Per Second: {{ latexGloves }}</p>
-          <p class="text-md text-center py-2">Nitrile Glove Rubs Per Second: {{ nitrileGloves * 2 }}</p>
-          <p class="text-md text-center py-2">Work Glove Rubs Per Second: {{ workGloves * 5 }}</p>
-          <p class="text-md text-center py-2">Total Rubs Per Second: {{ (autoScratcher/2) + (latexGloves) + (nitrileGloves * 2) + (workGloves * 5) }}</p>
+          <p class="text-md text-center py-2">Auto Belly Rubs Per Second: {{ autoScratcher }}</p>
+          <p class="text-md text-center py-2">Latex Glove Rubs Per Second: {{ latexGloves * 3 }}</p>
+          <p class="text-md text-center py-2">Nitrile Glove Rubs Per Second: {{ nitrileGloves * 5 }}</p>
+          <p class="text-md text-center py-2">Work Glove Rubs Per Second: {{ workGloves * 16 }}</p>
+          <p class="text-md text-center py-2">Welding Glove Rubs Per Second: {{ weldingGloves * 24 }}</p>
+          <p class="text-md text-center py-2">Total Rubs Per Second: {{ (autoScratcher) + (latexGloves*3) + (nitrileGloves * 5) + (workGloves * 16) + (weldingGloves * 24)}}</p>
+        </div>
+        <div v-if="luckyNumber == calcNumber" class="flex flex-col md:flex-row">
+          <button :disabled="luckyClicked == true" @click="bellyCounter *= 95; luckyClicked = true" class="px-20 py-3 mx-3 bg-green-400 rounded disabled:opacity-50">Multiply your belly rubs by 95</button>
+          <p class="self-center">Click it before it disappears</p>
+        </div>
+        <div v-else class="flex flex-col md:flex-row">
+          <button :disabled="luckyNumber != calcNumber" class="px-20 py-3 mx-3 bg-red-400 rounded">A bonus multiplier will appear here if you're lucky</button>
+          <p class="self-center">Click it before it disappears</p>
         </div>
       </div>
-      <div class="flex flex-col pt-10 pl-8">
+      <div class="flex flex-col pt-10 px-4">
         <h3 class="text-xl text-center pb-6" @click="autoScratch()">Increase your belly rubbing productivity</h3>
-        <div class="flex flex-row">
-          <button :disabled="bellyCounter < (5 * autoAdd)" @click="autoScratcher++; bellyCounter -= (5 * autoAdd); autoAdd = Math.floor(1.4*(autoAdd+1.7)); autoScratch(); calledAuto = true" class="px-20 py-3 m-3 bg-blue-200 rounded disabled:opacity-50">Auto Belly Rub: Cost {{ autoScratcherCost * autoAdd }}</button>
+        <div class="flex flex-col md:flex-row">
+          <button :disabled="bellyCounter < (5 * autoAdd)" @click="autoScratcher++; bellyCounter -= (5 * autoAdd); autoAdd = Math.floor(1.2*(autoAdd+1.7)); autoScratch(); calledAuto = true" class="px-20 py-3 m-3 bg-blue-200 rounded disabled:opacity-50">Auto Belly Rub: Cost {{ autoScratcherCost * autoAdd }}</button>
           <p class="self-center">Auto Belly Rubs: {{ autoScratcher }}</p>
         </div>
-        <div class="flex flex-row">
-          <button :disabled="bellyCounter < (10+postAdd)" @click="latexGloves++; bellyCounter -= (10 + postAdd); postAdd = Math.floor(1.8*(postAdd+3)); autoLatex(); calledLatex = true" class="px-20 py-3 m-3 bg-blue-200 rounded disabled:opacity-50">Latex Gloves: Cost {{ latexCost + postAdd }}</button>
+        <div class="flex flex-col md:flex-row">
+          <button :disabled="bellyCounter < (10+postAdd)" @click="latexGloves++; bellyCounter -= (10 + postAdd); postAdd = Math.floor(1.6*(postAdd+3)); autoLatex(); calledLatex = true" class="px-20 py-3 m-3 bg-blue-200 rounded disabled:opacity-50">Latex Gloves: Cost {{ latexCost + postAdd }}</button>
           <p class="self-center">Latex Gloves: {{ latexGloves }}</p>
         </div>
-        <div class="flex flex-row">
-          <button :disabled="bellyCounter < (45+nitrileAdd)" @click="nitrileGloves++; bellyCounter -= (45 + nitrileAdd); nitrileAdd = Math.floor(2.3*(nitrileAdd+3)); autoNitrile(); calledNitrile = true" class="px-20 py-3 m-3 bg-blue-200 rounded disabled:opacity-50">Nitrile Gloves: Cost {{ nitrileCost + nitrileAdd }}</button>
+        <div class="flex flex-col md:flex-row">
+          <button :disabled="bellyCounter < (45+nitrileAdd)" @click="nitrileGloves++; bellyCounter -= (45 + nitrileAdd); nitrileAdd = Math.floor(2*(nitrileAdd+3)); autoNitrile(); calledNitrile = true" class="px-20 py-3 m-3 bg-blue-200 rounded disabled:opacity-50">Nitrile Gloves: Cost {{ nitrileCost + nitrileAdd }}</button>
           <p class="self-center">Nitrile Gloves: {{ nitrileGloves }}</p>
         </div>
-        <div class="flex flex-row">
-          <button :disabled="bellyCounter < (95 + workAdd)" @click="workGloves++; bellyCounter -= (95 + workAdd); workAdd = Math.floor(2.8*(workAdd+5)); autoWork(); calledWork = true" class="px-20 py-3 m-3 bg-blue-200 rounded disabled:opacity-50">Work Gloves: Cost {{ workCost + workAdd }}</button>
+        <div class="flex flex-col md:flex-row">
+          <button :disabled="bellyCounter < (95 + workAdd)" @click="workGloves++; bellyCounter -= (95 + workAdd); workAdd = Math.floor(2.2*(workAdd+5)); autoWork(); calledWork = true" class="px-20 py-3 m-3 bg-blue-200 rounded disabled:opacity-50">Work Gloves: Cost {{ workCost + workAdd }}</button>
           <p class="self-center">Work Gloves: {{ workGloves }}</p>
+        </div>
+        <div class="flex flex-col md:flex-row">
+          <button :disabled="bellyCounter < (175 + weldingAdd)" @click="weldingGloves++; bellyCounter -= (175 + weldingAdd); weldingAdd = Math.floor(2.9*(weldingAdd+7)); autoWelding(); calledWelding = true" class="px-20 py-3 m-3 bg-blue-200 rounded disabled:opacity-50">Welding Gloves: Cost {{ weldingCost + weldingAdd }}</button>
+          <p class="self-center">Welding Gloves: {{ weldingGloves }}</p>
         </div>
       </div>
     </div>
@@ -52,6 +65,9 @@
         workGloves: 0,
         workAdd: 0,
         workCost: 95,
+        weldingGloves: 0,
+        weldingAdd: 0,
+        weldingCost: 175,
         autoScratcher: 0,
         autoScratcherCost: 5,
         autoAdd: 1,
@@ -59,9 +75,13 @@
         calledLatex: false,
         calledNitrile: false,
         calledWork: false,
+        calledWelding: false,
         seconds: 0,
         isAuto: true,
-        interval: 2000,
+        interval: 1000,
+        luckyNumber: 5,
+        calcNumber: 0,
+        luckyClicked: false,
       }
     },
 
@@ -79,7 +99,7 @@
       autoLatex(){
         if (this.calledLatex == false){
           setInterval(() => {
-            this.bellyCounter += (this.latexGloves*2)
+            this.bellyCounter += (this.latexGloves*3)
           }, this.interval)
         } else {
           this.calledLatex == true
@@ -89,7 +109,7 @@
       autoNitrile(){
         if (this.calledNitrile == false){
           setInterval(() => {
-            this.bellyCounter += (this.nitrileGloves*4)
+            this.bellyCounter += (this.nitrileGloves*5)
           }, this.interval)
         } else {
           this.calledNitrile == true
@@ -99,14 +119,33 @@
       autoWork(){
         if (this.calledWork == false){
           setInterval(() => {
-            this.bellyCounter += (this.workGloves*10)
+            this.bellyCounter += (this.workGloves*16)
           }, this.interval)
         } else {
           this.calledWork == true
         }
+      },
+
+      autoWelding(){
+        if (this.calledWelding == false){
+          setInterval(() => {
+            this.bellyCounter += (this.weldingGloves*24)
+          }, this.interval)
+        } else {
+          this.calledWelding == true
+        }
       }
 
 
+    },
+
+    created: function(){
+      setInterval(() => {
+        let luckyNumber = 5
+        let calcNumber = Math.floor(Math.random() * 5 + 1)
+        this.calcNumber = calcNumber
+        this.luckyClicked = false
+      }, 4000)
     }
   })
   </script>
